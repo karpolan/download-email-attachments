f98c8422920f967d185af7d55238b705a3e60808
Anton Karpenko <karpolan@karpolan.com>
2019-09-16 15:51:56 +0200

    Empty --since prameter is fixed

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ module.exports = function(config, callback) {
 	// Note: "$" signs in password should be escaped as "\$"
 	const account = typeof config.account === 'string' ? parseImapAccountString(config.account) : config.account;
 	const directory = config.directory ? normalizeDirectoryPath(config.directory) : DEFAULT_TARGET;
+	// The "since" should be set as a valide Date value
+	const since = config.since ? new Date(config.since) : 0; // Since 01 January, 1970 UTC by default
 
 	const args = {
 		invalidChars: config.invalidChars || /[^A-Za-z\d-_\.]/g, // /a^/g for everything
@@ -25,12 +27,12 @@ module.exports = function(config, callback) {
 		directory: directory,
 		filenameTemplate: config.filenameTemplate || DEFAULT_FILENAME_TEMPLATE,
 		filenameFilter: config.filenameFilter,
-		since: config.since,
+		since,
 		keepalive: config.keepalive,
 		lastSyncId: config.lastSyncId || 0,
 		uidvalidity: config.uidvalidity,
 		timeout: config.timeout || DEFAULT_TIMEOUT,
-		log: config.log || console.log,
+		log: config.log || console, // Used as args.log.info(), args.log.error(), and so on.
 	};
 
 	if (config.ssl === false) {
@@ -39,10 +41,7 @@ module.exports = function(config, callback) {
 		args.ssl = true;
 	}
 
-	args.log.info(
-		`Downloading attachments for "${args.username}" account to "${args.directory}" folder...`
-		//		'Downloading attachments for ' + args.username + ' since ' + args.since + ' to ' + args.directory + '...'
-	);
+	args.log.info(`Downloading attachments for "${args.username}" account to "${args.directory}" folder...`);
 
 	findEmails(args, callback);
 };
