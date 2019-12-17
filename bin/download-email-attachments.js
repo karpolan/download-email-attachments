5d938c4e31ea088da2e19d5fe495b20f24752764
Anton Karpenko <karpolan@karpolan.com>
2019-12-17 14:03:25 +0100

    npm audit. Some eslint directives added. Version and credentials added into package.json

--- bin/download-email-attachments.js
+++ bin/download-email-attachments.js
@@ -1,40 +1,40 @@
 #!/usr/bin/env node
 // console.log('process.argv:', process.argv);
 
-const onlyArguments = process.argv.slice(2);
-const argv = require('minimist')(onlyArguments, {});
+const onlyArguments = process.argv.slice(2)
+const argv = require('minimist')(onlyArguments, {})
 // console.dir(argv);
 
-const account = argv._[0];
-const directory = argv.directory; // argv['directory'];
-const filenameFilter = argv['filename-filter'] && new RegExp(argv['filename-filter']);
-const filenameTemplate = argv['filename-template'];
-const since = argv['since'];
-const timeout = argv['timeout'];
+const account = argv._[0]
+const directory = argv.directory // argv['directory'];
+const filenameFilter = argv['filename-filter'] && new RegExp(argv['filename-filter'])
+const filenameTemplate = argv['filename-template']
+const since = argv.since
+const timeout = argv.timeout
 
 const config = {
-	// ssl: true, // Shouldn't it be true by default ?
-	account,
-	directory,
-	filenameTemplate,
-	filenameFilter,
-	since,
-	timeout,
-	// Pass different consoles
-	log: {
-		info: console.info,
-		warn: console.warn,
-		debug: console.debug,
-		error: console.error,
-	},
-};
+  // ssl: true, // Shouldn't it be true by default ?
+  account,
+  directory,
+  filenameTemplate,
+  filenameFilter,
+  since,
+  timeout,
+  // Pass different consoles
+  log: {
+    info: console.info,
+    warn: console.warn,
+    debug: console.debug,
+    error: console.error
+  }
+}
 
-const downloadEmailAttachments = require('../index.js');
+const downloadEmailAttachments = require('../index.js')
 
-downloadEmailAttachments(config, function(error) {
-	if (error) {
-		console.log(JSON.stringify(error, null, 2));
-	}
-	console.log('done');
-	process.exit(0);
-});
+downloadEmailAttachments(config, function (error) {
+  if (error) {
+    console.log(JSON.stringify(error, null, 2))
+  }
+  console.log('done')
+  process.exit(0)
+})
--- index.js
+++ index.js
@@ -1,47 +1,48 @@
-const DEFAULT_PORT = 993;
-const DEFAULT_TARGET = './files';
-const DEFAULT_FILENAME_TEMPLATE = '{filename}';
-const DEFAULT_TIMEOUT = 1000;
+const DEFAULT_PORT = 993
+const DEFAULT_TARGET = './files'
+const DEFAULT_FILENAME_TEMPLATE = '{filename}'
+const DEFAULT_TIMEOUT = 1000
 
-const normalizeDirectoryPath = require('./lib/helpers/normalize-directory-path');
-const parseImapAccountString = require('./lib/helpers/parse-imap-account-string');
+const normalizeDirectoryPath = require('./lib/helpers/normalize-directory-path')
+const parseImapAccountString = require('./lib/helpers/parse-imap-account-string')
 
-const findEmails = require('./lib/find-emails');
+const findEmails = require('./lib/find-emails')
 
 module.exports = async (config, callback) => {
-	// console.log('index() config:', config);
+  // console.log('index() config:', config);
 
-	// Note: "$" signs in password should be escaped as "\$"
-	const account = typeof config.account === 'string' ? parseImapAccountString(config.account) : config.account;
-	const directory = config.directory ? normalizeDirectoryPath(config.directory) : DEFAULT_TARGET;
-	// The "since" should be set as a valide Date value
-	const since = config.since ? new Date(config.since) : 0; // Since 01 January, 1970 UTC by default
+  // Note: "$" signs in password should be escaped as "\$"
+  const account = typeof config.account === 'string' ? parseImapAccountString(config.account) : config.account
+  const directory = config.directory ? normalizeDirectoryPath(config.directory) : DEFAULT_TARGET
+  // The "since" should be set as a valide Date value
+  const since = config.since ? new Date(config.since) : 0 // Since 01 January, 1970 UTC by default
 
-	const args = {
-		invalidChars: config.invalidChars || /[^A-Za-z\d-_\.]/g, // /a^/g for everything
-		username: account.username,
-		password: account.password,
-		attachmentHandler: config.attachmentHandler,
-		host: account.host,
-		port: account.port || DEFAULT_PORT,
-		directory: directory,
-		filenameTemplate: config.filenameTemplate || DEFAULT_FILENAME_TEMPLATE,
-		filenameFilter: config.filenameFilter,
-		since,
-		keepalive: config.keepalive,
-		lastSyncId: config.lastSyncId || 0,
-		uidvalidity: config.uidvalidity,
-		timeout: config.timeout || DEFAULT_TIMEOUT,
-		log: config.log || console, // Used as args.log.info(), args.log.error(), and so on.
-	};
+	const DEFAULT_INVALID_CHARS = /[^A-Za-z\d-_\.]/g; // eslint-disable-line 
+  const args = {
+    invalidChars: config.invalidChars || DEFAULT_INVALID_CHARS, // /a^/g for everything
+    username: account.username,
+    password: account.password,
+    attachmentHandler: config.attachmentHandler,
+    host: account.host,
+    port: account.port || DEFAULT_PORT,
+    directory: directory,
+    filenameTemplate: config.filenameTemplate || DEFAULT_FILENAME_TEMPLATE,
+    filenameFilter: config.filenameFilter,
+    since,
+    keepalive: config.keepalive,
+    lastSyncId: config.lastSyncId || 0,
+    uidvalidity: config.uidvalidity,
+    timeout: config.timeout || DEFAULT_TIMEOUT,
+    log: config.log || console // Used as args.log.info(), args.log.error(), and so on.
+  }
 
-	if (config.ssl === false) {
-		args.ssl = false;
-	} else {
-		args.ssl = true;
-	}
+  if (config.ssl === false) {
+    args.ssl = false
+  } else {
+    args.ssl = true
+  }
 
-	args.log.info(`Downloading attachments for "${args.username}" account to "${args.directory}" folder...`);
+  args.log.info(`Downloading attachments for "${args.username}" account to "${args.directory}" folder...`)
 
-	await findEmails(args, callback);
-};
+  await findEmails(args, callback)
+}
